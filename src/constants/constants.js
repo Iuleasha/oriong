1948b3f63c1e8380b50963f5e9573e3281a05a8b
Igor Kalganov <k.i.a@mail.ru>
2021-06-01 13:56:16 +0300

    добавлены svg

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,14 +3,14 @@ import art4530 from "../img/products/4530-nitril-kraga.png";
 import art180 from "../img/products/180-pvh-moroz-rezinka.png";
 import art480 from "../img/products/480-pvh-moroz-kraga.png";
 import art790 from "../img/products/790-pvh-moroz-udlin-rezinka.png";
-import standardEn4441 from "../img/standarts/EN4441.PNG";
-import standardEn4342 from "../img/standarts/EN4342.PNG";
-import standardEn511 from "../img/standarts/EN511.PNG";
+import standardEn4441 from "../img/standarts/EN388-4441.svg";
+import standardEn4342 from "../img/standarts/EN388-4342.svg";
+import standardEn511 from "../img/standarts/EN511.svg";
 import standardEac from "../img/standarts/EAC.svg";
 import standardI from "../img/standarts/I.svg";
 import standardCe from "../img/standarts/CE-catII.svg";
-import standardAJKL from "../img/standarts/ISO-AJKL.PNG";
-import standardAkl from "../img/standarts/ISO-AKL.PNG";
+import standardAJKL from "../img/standarts/ISO-AJKL.svg";
+import standardAkl from "../img/standarts/ISO-AKL.svg";
 
 export {
   art1530,
